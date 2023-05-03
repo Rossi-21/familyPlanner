e40ea4a4e0e816a9f626a112a69e2b7293f02d35
Rossi21 <rossi21@gmail.com>
2023-05-02 22:03:48 -0700

    first commit

---
+++ src/main/java/com/rossi21/familyPlanner/FamilyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.rossi21.familyPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rossi21/familyPlanner/FamilyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossi21.familyPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
